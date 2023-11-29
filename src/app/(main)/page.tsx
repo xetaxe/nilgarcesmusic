7f06fee9f075943f058c4ef9ab59c5f2e0b36004
Nil <xetaxe@gmail.com>
2023-11-29 19:52:23 +0100

    Add serve + establish img srcset logic

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,8 +7,14 @@ import { Sponsors } from '@/components/Sponsors'
 export default function Home() {
   return (
     <>
+      {/* <img
+        srcSet="/img/dianne_guilianelli-small.jpg 600w, /img/dianne_guilianelli-mid.jpg 1024w, /img/dianne_guilianelli-large.jpg 1120w"
+        // sizes="(min-width: 1024px) 1024px, (min-width: 600px) 700px, 400px"
+        src="/img/dianne_guilianelli-mid.jpg"
+        alt=""
+      /> */}
       <Hero />
-      <Speakers />
+      {/* <Speakers /> */}
       <Schedule />
       <Sponsors />
       <Newsletter />
