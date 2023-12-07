99952aa55ef34f88508cf040c78178690ad680b9
Nil <xetaxe@gmail.com>
2023-12-07 22:02:37 +0100

    Delete unused default components

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,11 @@
 import { Hero } from '@/components/Hero'
-import { Newsletter } from '@/components/Newsletter'
-import { Schedule } from '@/components/Schedule'
 import { Bio } from '@/components/Bio'
-import { Sponsors } from '@/components/Sponsors'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Bio />
-      <Schedule />
-      <Sponsors />
-      <Newsletter />
     </>
   )
 }
