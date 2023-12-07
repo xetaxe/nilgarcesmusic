0f2dc8545fa2adefcdc4c1688545f35f638825f1
Nil <xetaxe@gmail.com>
2023-12-07 22:08:45 +0100

    Correct not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,30 @@
-// import { BackgroundImage } from '@/components/ui/BackgroundImage'
 import { Button } from '@/components/ui/Button'
 import { Container } from '@/components/layout/Container'
+import Image from 'next/image'
+import backgroundImage from '@/images/background2.jpg'
 
 export default function NotFound() {
   return (
     <div className="relative flex h-full items-center py-20 sm:py-36">
-      {/* <BackgroundImage className="-top-36 bottom-0" /> */}
+      <div className="absoluteinset-x-0 bottom-10 overflow-hidden">
+        <Image
+          className= "absolute object-cover object-top"
+          src={backgroundImage}
+          alt="El fred i la primavera cover image"
+          fill
+          priority
+          unoptimized
+        />
+      </div>
       <Container className="relative flex w-full flex-col items-center">
         <p className=" text-2xl tracking-tight text-blue-900">
           404
         </p>
-        <h1 className="mt-4  text-4xl font-medium tracking-tighter text-blue-600 sm:text-5xl">
-          Page not found
+        <h1 className="mt-4  text-4xl font-medium tracking-tighter text-logo-400 sm:text-5xl">
+          La pàgina no existeix
         </h1>
-        <p className="mt-4 text-lg tracking-tight text-blue-900">
-          Sorry, we couldn’t find the page you’re looking for.
-        </p>
         <Button href="/" className="mt-8">
-          Go back home
+          Vés a la pàgina principal
         </Button>
       </Container>
     </div>
