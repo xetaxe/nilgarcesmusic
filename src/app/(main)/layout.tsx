239ce0621de5c89b17ccde4ba6c685f7de9650ae
Nil <xetaxe@gmail.com>
2023-12-10 15:54:34 +0100

    Change footer colors to match logo color

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,19 +9,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </main>
       <footer className="bg-white">
         <Container>
-          <div className="border-gray-400 border-solid border-t mx-12">
+          <div className="border-logo-400 border-solid border-t mx-12">
           </div>
           <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
             <div className="flex justify-center space-x-6 md:order-2">
               {socialLinks.map((item) => (
-                <a key={item.name} href={item.href} target="_blank" className="text-gray-400 hover:text-gray-500">
+                <a key={item.name} href={item.href} target="_blank" className="text-logo-400">
                   <span className="sr-only">{item.name}</span>
                   <item.icon className="h-6 w-6" aria-hidden="true" />
                 </a>
               ))}
             </div>
             <div className="mt-8 md:order-1 md:mt-0">
-              <p className="text-center text-xs leading-5 text-gray-500">
+              <p className="text-center text-xs leading-5 text-logo-400">
                 &copy; {new Date().getFullYear()} Nil Garcés
               </p>
             </div>
