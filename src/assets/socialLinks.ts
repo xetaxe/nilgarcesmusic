8b972101b9447c99e0354980273962a8a6f7c3e2
Nil <xetaxe@gmail.com>
2023-12-10 15:26:49 +0100

    Add social links to header

--- src/assets/socialLinks.ts
+++ src/assets/socialLinks.ts
@@ -3,19 +3,19 @@ import { SpotifyIcon, AppleMusicIcon, InstagramIcon, YoutubeIcon, FacebookIcon }
 
 export const socialLinks = [
   {
-    name: "Spotify",
-    href: "https://open.spotify.com/intl-es/artist/0rvOkwdehM4VznxtpJwgC2",
-    icon: (props: SVGProps<SVGSVGElement>) => SpotifyIcon(props)
-  },
-  {
-    name: "Apple Music",
-    href: "https://music.apple.com/es/artist/nil-garc%C3%A9s/1436841437",
-    icon: (props: SVGProps<SVGSVGElement>) => AppleMusicIcon(props)
-  },
-  {
     name: "Instagram",
     href: "https://www.instagram.com/nil.garces/",
     icon: (props: SVGProps<SVGSVGElement>) => InstagramIcon(props)
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/nilgarces",
+    icon: (props: SVGProps<SVGSVGElement>) => FacebookIcon(props)
+  },
+  {
+    name: "Spotify",
+    href: "https://open.spotify.com/intl-es/artist/0rvOkwdehM4VznxtpJwgC2",
+    icon: (props: SVGProps<SVGSVGElement>) => SpotifyIcon(props)
   },
   {
     name: "YouTube",
@@ -23,8 +23,8 @@ export const socialLinks = [
     icon: (props: SVGProps<SVGSVGElement>) => YoutubeIcon(props)
   },
   {
-    name: "Facebook",
-    href: "https://www.facebook.com/nilgarces",
-    icon: (props: SVGProps<SVGSVGElement>) => FacebookIcon(props)
-  },
+    name: "Apple Music",
+    href: "https://music.apple.com/es/artist/nil-garc%C3%A9s/1436841437",
+    icon: (props: SVGProps<SVGSVGElement>) => AppleMusicIcon(props)
+  }
 ]
